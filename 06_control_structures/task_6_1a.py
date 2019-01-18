c30168b601b904eed3d3011d5bbb937d7add33ef
angolpow <angolpow@gmail.com>
2019-01-18 14:59:00 -0500

    6.1a done

--- 06_control_structures/task_6_1a.py
+++ 06_control_structures/task_6_1a.py
@@ -16,3 +16,34 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+
+IP = input('Enter IP address: ')
+TRY = "true"
+count = 0
+
+try:
+	IPSTR = IP.split('.')
+	FOCT = int(IPSTR[0])
+	
+	for i in IPSTR:
+		if int(i) / 255 > 1:
+			TRY = "false"
+		count += 1
+
+except:
+	TRY = "false"
+
+if TRY == "true" and count == 4 :
+	if IP == '255.255.255.255':
+		print("local broadcast")
+	elif IP == '0.0.0.0':
+		print('unassigned')
+	elif FOCT > 0 and FOCT < 224:
+		print('unicast')
+	elif FOCT > 223 and FOCT < 240:
+		print('multicast')
+	else:
+		print('unused')
+else:
+	print('Incorrect IPv4 address')
+
